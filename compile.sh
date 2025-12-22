ddb42c867d86dff95ce58edc2e2f8bf58ce5707f
Julio Werner <juliowernery@gmail.com>
2025-12-22 14:25:46 -0300

    Refactor compile.sh to skip specific source files during compilation

--- compile.sh
+++ compile.sh
@@ -6,7 +6,12 @@ COMPILE_FLAGS="-fbounds-check -Wuninitialized -O2 -static"
 LINK_FLAGS="-O2"
 
 # Compile all Fortran source files in the source directory
+gfortran -c $COMPILE_FLAGS "$SOURCE_DIR/modules.f90"
+gfortran -c $COMPILE_FLAGS "$SOURCE_DIR/grsm.f90"
 for src in "$SOURCE_DIR"/*.f90; do
+    if [ "$(basename "$src")" = "modules.f90" ] || [ "$(basename "$src")" = "grsm.f90" ]; then
+        continue
+    fi
     gfortran -c $COMPILE_FLAGS "$src"
 done
 
